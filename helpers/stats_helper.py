7bb4415e66a19a87916e5fc8bde57a21ead58355
alex501212 <56118464+alex501212@users.noreply.github.com>
2020-10-16 21:29:32 +0100

    level 1 sql py

--- helpers/stats_helper.py
+++ helpers/stats_helper.py
@@ -12,6 +12,11 @@ class StatsHelper():
         return result[0]
 
     # HINT: You can define more queries here, along with some python logic to calculate!
-    def calculate_another_stat(self):
-      all_rows = self.database.fetch_all("")
+    def get_top5(self):
+      all_rows = self.database.fetch_all('SELECT DISTINCT beer_name FROM reviews GROUP BY beer_name ORDER BY "review_taste" DESC LIMIT 5')
       return all_rows
+
+    def get_low5_ave_taste(self):
+      all_rows = self.database.fetch_all('SELECT DISTINCT beer_name, AVG(review_taste) FROM reviews GROUP BY beer_name ORDER BY AVG(review_overall) ASC LIMIT 5')
+      return all_rows
+
