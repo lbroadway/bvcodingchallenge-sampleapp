7ad95b45312a8db34731d782a4d1438f782ab9ce
Liam Broadway <broadway-l@ulster.ac.uk>
2020-10-16 21:30:50 +0100

    Update

--- application.py
+++ application.py
@@ -9,7 +9,7 @@ stats_helper = StatsHelper()
 @app.route('/')
 def homepage():
     # HINT: Pass variables through to the HTML using Flask - https://flask.palletsprojects.com/en/1.1.x/quickstart/#rendering-templates
-    return render_template('index.html', todays_date=datetime.now())
+    return render_template('index.html', todays_date=datetime.now(), overall_rating=get_average_overall_rating())
 
 # HINT: This could be your first statistic!
 def get_average_overall_rating():
